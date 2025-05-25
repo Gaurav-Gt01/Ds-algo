bf6015bd5f23a789951c9c761cdcbe7d476841f6
Gaurav Tatpate <gauravtatpate@gmail.com>
2025-05-25 11:13:23 +0530

    Strings

---
+++ Strings/stringVector.cpp
@@ -0,0 +1,11 @@
+# include <iostream>
+using namespace std;
+
+int main() {
+
+    vector<string> name = {"Gaurav" , "Aryan"} ;
+    string n = name[1] ;
+    cout<<name[1]<<endl<<n[1];
+
+    return 0;
+}
