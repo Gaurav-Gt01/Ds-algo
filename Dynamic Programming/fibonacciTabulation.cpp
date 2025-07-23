2c3a1490e0e0f347519d68e56fabc8c21ab2d5b7
Gaurav Tatpate <gauravtatpate@gmail.com>
2025-07-23 23:58:54 +0530

    Dp top down fibonacci down

---
+++ Dynamic Programming/fibonacciTabulation.cpp
@@ -0,0 +1,19 @@
+// Tabulation Approch of solving Fibonacci :
+
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1) return n;
+
+        int prev = 1 ;
+        int prev2 = 0 ;
+        int ans = -1 ;
+
+        for(int i = 2 ; i<=n ; i++){
+            ans = prev + prev2 ;
+            prev2 = prev ;
+            prev = ans ;
+        }
+        return ans ;
+    }
+};
